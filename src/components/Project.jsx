28ca73c1f2611d55d1f5600c281f518bb9552ba4
RCLobster <44736025+RCLobster@users.noreply.github.com>
2024-03-12 15:01:35 -0700

    css for home page / project component

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,10 +2,10 @@ import { Link } from 'react-router-dom';
 
 export default function Project({ title, url, description }) {
     return (
-        <div className="project-card flex-parent">
-            <h2>{title}</h2>
-            <p>{description}</p>
-            <Link to={url} target="_blank"><button className="btn">View Project</button></Link>
-        </div>
+        <Link to={url} target="_blank" className="project-card flex-parent">
+            <h2 >{title}</h2>
+            <p id="fadeAway">{description}</p>
+            <p id="viewProject">Click to View Project</p>
+        </Link>
     )
 };
