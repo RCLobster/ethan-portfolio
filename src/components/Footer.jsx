2c7dc1d6a6f14f9eb0ec63bfc82c03ab9ca9977a
RCLobster <44736025+RCLobster@users.noreply.github.com>
2024-03-14 11:42:47 -0700

    footer styling, added links to github and linkedin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,13 @@
 export default function Footer() {
     return (
-        <div className="flex-parent-footer">
-            <p className="footer">Git Link</p>
+        <div className="footer">
+            <a href="https://www.github.com/RCLobster" target='_blank'>
+                <img src="../public/images/github_icon.png" />
+            </a>
+
+            <a href="https://www.linkedin.com/in/ethanstone27/" target='_blank'>
+                <img className="linkedin" src="../public/images/linkedin_black.png" />
+            </a>
         </div>
     );
 }
