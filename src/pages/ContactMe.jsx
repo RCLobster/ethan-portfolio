4206e20ef8cc3891183c6a8fd9c22c2358be73e3
RCLobster <44736025+RCLobster@users.noreply.github.com>
2024-03-18 13:30:43 -0700

    removed .. from image links

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -3,11 +3,11 @@ function ContactMe() {
         <div className="contact-card flex-parent-contact">
             <h2>Contact Me</h2>
             <div className="contact-children">
-                <img src="../public/images/email_icon.png" title="icon by rukanicon" alt="email envelop icon" />
+                <img src="/public/images/email_icon.png" title="icon by rukanicon" alt="email envelop icon" />
                 <a href="mailto:esstone27@gmail.com">esstone27@gmail.com</a>
             </div>
             <div className="contact-children">
-                <img src="../public/images/circle-phone_icon.png" title="icon by UIcons" alt="phone icon" />
+                <img src="/public/images/circle-phone_icon.png" title="icon by UIcons" alt="phone icon" />
                 <p>310.710.2087</p>
             </div>
         </div>
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,10 +2,10 @@ export default function Resume() {
     return (
         <div className="flex-parent-resume resume-card">
             <h2>Ethan's Resume</h2>
-            <img className="resume-img" src="../public/images/StoneE_Resume_WebDev.png" alt="Ethan Stone's resume" />
+            <img className="resume-img" src="/public/images/StoneE_Resume_WebDev.png" alt="Ethan Stone's resume" />
             <div className="resume-download">
-                <a download="Ethan Stone's Resume" href="../public/pdf/StoneE_Resume_WebDev.pdf">
-                    <img className="download-icon" src="../public/icons/download_icon.svg" title="Icon by Baharat Icons" />
+                <a download="Ethan Stone's Resume" href="/public/pdf/StoneE_Resume_WebDev.pdf">
+                    <img className="download-icon" src="/public/icons/download_icon.svg" title="Icon by Baharat Icons" />
                 </a>
             </div>
         </div>
