5a1ec14325f12e11b7a3a5c21f54a888747d7e75
RCLobster <44736025+RCLobster@users.noreply.github.com>
2024-03-18 12:51:40 -0700

    more css tweaks

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <div className="footer">
             <a href="https://www.github.com/RCLobster" target='_blank'>
-                <img src="../public/images/github_icon.png" alt="github icon" />
+                <img className="github" src="../public/images/github_icon.png" alt="github icon" />
             </a>
 
             <a href="https://www.linkedin.com/in/ethanstone27/" target='_blank'>
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -5,7 +5,7 @@ export default function Resume() {
             <img className="resume-img" src="../public/images/StoneE_Resume_WebDev.png" alt="Ethan Stone's resume" />
             <div className="resume-download">
                 <a download="Ethan Stone's Resume" href="../public/pdf/StoneE_Resume_WebDev.pdf">
-                    <img src="../public/images/download_icon.png" title="Icon by Baharat Icons" />
+                    <img className="download-icon" src="../public/icons/download_icon.svg" title="Icon by Baharat Icons" />
                 </a>
             </div>
         </div>
