9e0efbcb8ae4959765dbc791ad3645867b688c5d
RCLobster <44736025+RCLobster@users.noreply.github.com>
2024-03-19 14:05:44 -0700

    added .. to image links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,11 @@ export default function Footer() {
     return (
         <div className="footer">
             <a href="https://www.github.com/RCLobster" target='_blank'>
-                <img className="github" src="/public/images/github_icon.png" alt="github icon" />
+                <img className="github" src="../public/images/github_icon.png" alt="github icon" />
             </a>
 
             <a href="https://www.linkedin.com/in/ethanstone27/" target='_blank'>
-                <img className="linkedin" src="/public/images/linkedin_black.png" alt="linkedIn icon" />
+                <img className="linkedin" src="../public/images/linkedin_black.png" alt="linkedIn icon" />
             </a>
         </div>
     );
